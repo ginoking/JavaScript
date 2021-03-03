7c1b438ab0252d6e45007cbb932b8b47e5d7bcba
gino king <gino_king@oright.com.tw>
2021-03-03 10:34:17 +0800

    drag and drop

---
+++ drag_drop/script.js
@@ -0,0 +1,48 @@
+const fill = document.querySelector('.fill')
+const empies = document.querySelectorAll('.empty')
+
+fill.addEventListener('dragstart', dragStart)
+fill.addEventListener('dragend', dragEnd)
+
+
+// for(const empty of empies) {
+// 	empty.addEventListener('dragover', dragOver)
+// 	empty.addEventListener('dragenter', dragEnter)
+// 	empty.addEventListener('dragleave', dragLeave)
+// 	empty.addEventListener('drop', dragDrop)
+// }
+
+
+empies.forEach(empty => {
+	empty.addEventListener('dragover', dragOver)
+	empty.addEventListener('dragenter', dragEnter)
+	empty.addEventListener('dragleave', dragLeave)
+	empty.addEventListener('drop', dragDrop)
+})
+
+function dragStart() {
+	this.className += ' hold'
+	setTimeout(() => this.className = 'invisible', 0)
+}
+
+function dragEnd() {
+	this.className = 'fill'
+}
+
+function dragOver(e) {
+	e.preventDefault()
+}
+
+function dragLeave() {
+	this.className = 'empty'
+}
+
+function dragEnter(e) {
+	//e.preventDefault()
+	this.className += ' hovered'
+}
+
+function dragDrop() {
+	this.className = 'empty'
+	this.append(fill)
+}
